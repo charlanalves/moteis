6fcefea79753e5681346c68deac2023962d06e89
charlanalves <charlan.job@gmail.com>
2017-03-04 19:12:29 -0300

    init

---
+++ logs/error.php
@@ -0,0 +1,10 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-06-16 14:24:28 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2015-06-16T14:24:28+00:00	INFO 212.109.165.124	joomlafailure	Username and password do not match or you do not have an account yet.
+2017-03-03T00:23:41+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2017-03-03T00:23:55+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2017-03-03T00:24:00+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
