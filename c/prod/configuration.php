19d0c9bbb88235998a633b9d2d1b708a090f1280
charlanalves <charlan.job@gmail.com>
2017-03-12 14:54:30 -0300

    configuracao dos logs e tmp

--- c/prod/configuration.php
+++ c/prod/configuration.php
@@ -54,8 +54,8 @@ class JConfig {
 	public $sef_suffix = '0';
 	public $unicodeslugs = '0';
 	public $feed_limit = '10';
-	public $log_path = 'C:\\SERVER\\www\\moteis/logs';
-	public $tmp_path = 'C:\\SERVER\\www\\moteis/tmp';
+	public $log_path = '/var/www/html/cashback/dev/moteis/logs';
+	public $tmp_path = '/var/www/html/cashback/dev/moteis/tmp';
 	public $lifetime = '15';
 	public $session_handler = 'database';
 	public $memcache_persist = '1';
